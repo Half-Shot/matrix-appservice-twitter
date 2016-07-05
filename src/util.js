a6f603d26f2d8dd2725a8c510f00fe03816bb828
Will Hunt <half-shot@molrams.com>
2016-07-05 14:54:58 +0100

    Added downloadFile to util.
    * Intelligently decide between https and http.
    * Fallback to the node-mime library when a content-type is not given

--- src/util.js
+++ src/util.js
@@ -1,17 +1,68 @@
 var https   = require('https');
+var http   = require('http');
+
 var Buffer  = require("buffer").Buffer;
 var log     = require('npmlog');
+var mime    = require('mime');
+
 
 /*
   This function will take a URL, upload it to Matrix and return the corresponding
   MXC url in a Promise. The content will be uploaded on the users behalf using
   the ID, or the AS bot if set to null.
 */
+
+function downloadFile(url){
+  return new Promise((resolve, reject) => {
+
+    if(url.startsWith("https")){
+      var ht = https;
+    }
+    else {
+      var ht = http;
+    }
+
+    ht.get((url), (res) => {
+        var size = parseInt(res.headers["content-length"]);
+        var buffer = Buffer.alloc(size);
+        var bsize = 0;
+
+        res.on('data', (d) => {
+            d.copy(buffer, bsize);
+            bsize += d.length;
+        });
+
+        res.on('error', () => {
+            reject("Failed to download.");
+        });
+
+        res.on('end', () => {
+            resolve(buffer);
+        });
+    });
+  });
+}
+
 function uploadContentFromUrl(bridge, url, id = null, name = null) {
     var contenttype;
     return new Promise((resolve, reject) => {
-        https.get((url), (res) => {
-            contenttype = res.headers["content-type"];
+
+        if(url.startsWith("https")){
+          var ht = https;
+        }
+        else {
+          var ht = http;
+        }
+
+        ht.get((url), (res) => {
+            if(res.headers.hasOwnProperty("content-type")){
+              contenttype = res.headers["content-type"];
+            }
+            else{
+              log.info("No content-type given by server, guessing based on file name.");
+              contenttype = mime.lookup(url);
+            }
+
             if (name == null) {
                 name = url.split("/");
                 name = name[name.length - 1];
@@ -50,5 +101,6 @@ function uploadContentFromUrl(bridge, url, id = null, name = null) {
 }
 
 module.exports = {
-    uploadContentFromUrl: uploadContentFromUrl
+    uploadContentFromUrl: uploadContentFromUrl,
+    downloadFile: downloadFile
 }
