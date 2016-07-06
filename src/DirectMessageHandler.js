2b4864456d2b58eafc86b34c69c665a044c18a56
Will Hunt <half-shot@molrams.com>
2016-07-06 16:49:52 +0100

    Removed stubs

--- src/DirectMessageHandler.js
+++ src/DirectMessageHandler.js
@@ -7,26 +7,10 @@ var DirectMessage = function (bridge, twitter) {
   this.twitter = twitter;
 }
 
-
-DirectMessage.prototype.processInvite = function (event, request, context) {
-  return;//No invites
-}
-
 DirectMessage.prototype.processMessage = function (event, request, context) {
-    //this.twitter.send_matrix_event_as_tweet(event,context.senders.remote,context.rooms.remote);
     if(event.content.msgtype == "m.text"){
       this.twitter.send_dm(event.sender,event.room_id,event.content.body);
     }
-}
-
-DirectMessage.prototype.processEvent = function (event, request, context) {
-  // if(event.type == "m.room.aliases" && event.sender.startsWith("@twitbot")){
-  //   this.twitter.add_hashtag_feed(
-  //     context.rooms.remote.roomId.substr("hashtag_".length),
-  //     context.rooms.matrix,
-  //     context.rooms.remote
-  //   );
-  // }
 }
 
 DirectMessage.prototype.processAliasQuery = function(name){
