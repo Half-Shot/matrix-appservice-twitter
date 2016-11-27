40b8de66f7faf13931b21ba7fbc95c8f35c96ab9
Will Hunt <half-shot@molrams.com>
2016-11-27 15:42:35 +0000

    Add more tests for util.js

--- src/util.js
+++ src/util.js
@@ -1,9 +1,9 @@
-var https   = require('https');
-var http   = require('http');
+const https   = require('https');
+const http   = require('http');
 
-var Buffer  = require("buffer").Buffer;
-var log     = require('npmlog');
-var mime    = require('mime');
+const Buffer  = require("buffer").Buffer;
+const log     = require('npmlog');
+const mime    = require('mime');
 const Promise = require('bluebird');
 
 /**
@@ -20,25 +20,23 @@ const Promise = require('bluebird');
 function downloadFile (url) {
   return new Promise((resolve, reject) => {
 
-    var ht = url.startsWith("https") ? https : http;
-
-    ht.get((url), (res) => {
-      var size = parseInt(res.headers["content-length"]);
-      var buffer = Buffer.alloc(size);
-      var bsize = 0;
+    const ht = url.startsWith("https") ? https : http;
+    const req = ht.get((url), (res) => {
+      var buffer = Buffer.alloc(0);
+      if(res.statusCode !== 200) {
+        reject(`Non 200 status code (${res.statusCode}) `)
+      }
 
       res.on('data', (d) => {
-        d.copy(buffer, bsize);
-        bsize += d.length;
-      });
-
-      res.on('error', () => {
-        reject("Failed to download.");
+        buffer = Buffer.concat([buffer, d]);
       });
 
       res.on('end', () => {
         resolve(buffer);
       });
+    });
+    req.on('error', (err) =>{
+      reject(`Failed to download. ${err.code}`);
     });
   });
 }
@@ -56,58 +54,47 @@ function downloadFile (url) {
  */
 function uploadContentFromUrl (bridge, url, id, name) {
   var contenttype;
+  let size;
   id = id || null;
   name = name || null;
-  var size;
   return new Promise((resolve, reject) => {
 
-    var ht = url.startsWith("https") ? https : http;
+    const ht = url.startsWith("https") ? https : http;
+    const req = ht.get((url), (res) => {
+      let buffer = Buffer.alloc(0);
 
-    ht.get((url), (res) => {
       if(res.headers.hasOwnProperty("content-type")) {
         contenttype = res.headers["content-type"];
       }
       else{
-        log.info("No content-type given by server, guessing based on file name.");
+        log.verbose("UploadContent", "No content-type given by server, guessing based on file name.");
         contenttype = mime.lookup(url);
       }
 
-      if (name == null) {
+      if (name === null) {
         name = url.split("/");
         name = name[name.length - 1];
       }
-      size = parseInt(res.headers["content-length"]);
-      if(isNaN(size)) {
-        reject("Content-Length was not an integer, which is weird.");
-        return;
-      }
-      var buffer;
-      if(Buffer.alloc) {//Since 5.10
-        buffer = Buffer.alloc(size);
-      }
-      else {//Deprecated
-        buffer = new Buffer(size);
-      }
 
-      var bsize = 0;
       res.on('data', (d) => {
-        d.copy(buffer, bsize);
-        bsize += d.length;
+        buffer = Buffer.concat([buffer, d]);
       });
-      res.on('error', () => {
-        reject("Failed to download.");
-      });
+
       res.on('end', () => {
         resolve(buffer);
       });
-    })
+    });
+    req.on('error', (err) =>{
+      reject(`Failed to download. ${err.code}`);
+    });
   }).then((buffer) => {
-    if(id == null || typeof id == "string") {
+    size = buffer.length;
+    if(id === null || typeof id === "string") {
       id = bridge.getIntent(id);
     }
     return id.getClient().uploadContent({
       stream: buffer,
-      name: name,
+      name,
       type: contenttype
     });
   }).then((response) => {
@@ -118,8 +105,9 @@ function uploadContentFromUrl (bridge, url, id, name) {
       size
     };
   }).catch(function (reason) {
-    log.error("UploadContent", "Failed to upload content:\n%s", reason)
-  })
+    log.error("UploadContent", "Failed to upload content:\n%s", reason);
+    throw reason;
+  });
 
 }
 
