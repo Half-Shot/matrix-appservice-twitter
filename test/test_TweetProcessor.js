17fc22c9736a5555ce7802e683e4412d105aca50
Will Hunt <half-shot@molrams.com>
2017-01-15 17:10:17 +0000

    Add tests for tweet_to_matrix_content

--- test/test_TweetProcessor.js
+++ test/test_TweetProcessor.js
@@ -55,6 +55,7 @@ describe('TweetProcessor', function () {
     it('will start ', function () {
       processor._process_head_of_msg_queue = () => {};
       processor.start();
+      processor.stop();
     });
   });
   describe('_process_head_of_msg_queue', function () {
@@ -91,47 +92,117 @@ describe('TweetProcessor', function () {
         assert.isTrue(processor.msg_queue.length === 0);
       });
     });
-    describe('_tweet_expand_urls', function () {
-      it('will not modify a urlless tweet.', function () {
-        const text = processor._tweet_expand_urls("Words, words and more words", []);
-        assert.equal(text, "Words, words and more words");
+  });
+  describe('tweet_to_matrix_content', function () {
+    it('will convert html special chars to regular characters.', function () {
+      const mx_content = processor.tweet_to_matrix_content({
+        text: "&lt;3",
+        entities: { },
+        user: {
+
+        }
       });
-      it('will modify a tweet with a single url.', function () {
-        const text = processor._tweet_expand_urls("Words,http://short words and more words", [{
-          expanded_url: "http://example.com",
-          indices: [6, 18]
-        }]);
-        assert.equal(text, "Words,http://example.com words and more words");
+      const mx_content2 = processor.tweet_to_matrix_content({
+        text: "<3",
+        entities: { },
+        user: {
+
+        }
       });
-      it('will modify a tweet with two urls.', function () {
-        const text = processor._tweet_expand_urls("Words,http://short words and more words with http://e.shorter", [{
+      assert.equal(mx_content.body, "<3");
+      assert.equal(mx_content2.body, "<3");
+    });
+    it('will put hashtags in content', function () {
+      const mx_content = processor.tweet_to_matrix_content({
+        text: "#foo, #bar and #baz",
+        entities: {
+          hashtags: [
+            {
+              indices: [0, 3],
+              text: "foo"
+            },
+            {
+              indices: [6, 10],
+              text: "bar"
+            },
+            {
+              indices: [15, 19],
+              text: "baz"
+            }
+          ]
+        },
+        user: {
+
+        }
+      });
+      assert.sameMembers(mx_content.tags, ["foo", "bar", "baz"]);
+    });
+    it('will put metadata in content', function () {
+      const mx_content = processor.tweet_to_matrix_content({
+        full_text: "This is a fulltext.",
+        created_at: "Today",
+        favorite_count: 5,
+        retweet_count: 42,
+        id_str: "820675371330916353",
+        entities: { },
+        user: {
+          screen_name: "Half_Shot",
+          id_str: "366675043"
+        },
+        _retweet_info: "This would be a retweet thing, but here is some content"
+      });
+      assert.equal(mx_content.body, "This is a fulltext.");
+      assert.equal(mx_content.created_at, "Today");
+      assert.equal(mx_content.likes, 5);
+      assert.equal(mx_content.reblogs, 42);
+      assert.equal(mx_content.tweet_id, "820675371330916353");
+      assert.equal(mx_content.external_url, "https://twitter.com/Half_Shot/status/820675371330916353");
+      assert.equal(mx_content.created_at, "Today");
+      assert.equal(mx_content.retweet, "This would be a retweet thing, but here is some content");
+    });
+    // full_text or text
+  });
+
+  describe('_tweet_expand_urls', function () {
+    it('will not modify a urlless tweet.', function () {
+      const text = processor._tweet_expand_urls("Words, words and more words", []);
+      assert.equal(text, "Words, words and more words");
+    });
+    it('will modify a tweet with a single url.', function () {
+      const text = processor._tweet_expand_urls("Words,http://short words and more words", [{
+        expanded_url: "http://example.com",
+        indices: [6, 18]
+      }]);
+      assert.equal(text, "Words,http://example.com words and more words");
+    });
+    it('will modify a tweet with two urls.', function () {
+      const text = processor._tweet_expand_urls("Words,http://short words and more words with http://e.shorter", [{
+        expanded_url: "http://example.com",
+        indices: [6, 18]
+      }, {
+        expanded_url: "https://evenshorter.com",
+        indices: [45, 61]
+      }]);
+      assert.equal(text, "Words,http://example.com words and more words with https://evenshorter.com");
+    });
+    it('will modify a tweet with three urls.', function () {
+      const text = processor._tweet_expand_urls(
+        "Words,http://short words and http://m.short more words with http://e.shorter",
+        [{
           expanded_url: "http://example.com",
           indices: [6, 18]
         }, {
+          expanded_url: "http://penguins.com",
+          indices: [29, 43]
+        }, {
           expanded_url: "https://evenshorter.com",
-          indices: [45, 61]
-        }]);
-        assert.equal(text, "Words,http://example.com words and more words with https://evenshorter.com");
-      });
-      it('will modify a tweet with three urls.', function () {
-        const text = processor._tweet_expand_urls(
-          "Words,http://short words and http://m.short more words with http://e.shorter",
-          [{
-            expanded_url: "http://example.com",
-            indices: [6, 18]
-          }, {
-            expanded_url: "http://penguins.com",
-            indices: [29, 43]
-          }, {
-            expanded_url: "https://evenshorter.com",
-            indices: [60, 76]
-          }]
-        );
-        assert.equal(
-          text,
-          "Words,http://example.com words and http://penguins.com more words with https://evenshorter.com"
-        );
-      });
+          indices: [60, 76]
+        }]
+      );
+      assert.equal(
+        text,
+        "Words,http://example.com words and http://penguins.com more words with https://evenshorter.com"
+      );
     });
   });
 });
